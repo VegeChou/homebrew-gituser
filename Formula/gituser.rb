2cf799b0bce3d3abfec0b275320fe9706053e42a
Devin <devin@seethingx.com>
2025-08-07 13:42:03 +0800

    update v0.1.0 sha256

--- Formula/gituser.rb
+++ Formula/gituser.rb
@@ -2,7 +2,7 @@ class Gituser < Formula
   desc "Manage multiple Git user profiles easily"
   homepage "https://github.com/VegeChou/gituser"
   url "https://github.com/VegeChou/gituser/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7f2d4a1f3c67f31467a168b83b650dd772963ab3edb0a348b2218612eb3d3dcf"
+  sha256 "60fe62be46495251eb8a8c365020c6751d6806e7051f1abdc8918a272cffd35a"
   license "MIT"
 
   depends_on "bash"
